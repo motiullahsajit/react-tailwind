4154d0626d639d0c978f0b8f7c30c4428e91c131
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-06-09 13:25:52 +0600

    mobile view done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <div className="min-h-screen w-screen flex justify-center items-center flex-col-reverse" style={{
+      background: 'linear-gradient(to bottom, #FOF4FD, #A1A3BA)'
+    }}>
+      <div className='mt-5 text-center'>
+        <h1 className="font-black text-5xl">Fresh Healthy <br />
+          <span className="font-light">Delicious Salads</span>
+        </h1>
+        <p className="text-base text-gray-700">We made fresh and healthy foods</p>
+        <span className='inline-block bg-indigo-500 px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wide'>Explore</span>
+      </div>
+      <img src={process.env.PUBLIC_URL + "/assets/food.svg"} alt="food" className="w-screen" />
+    </div >
   );
 }
 
